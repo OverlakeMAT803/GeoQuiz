f8b51c9f5dccb6f777d32c1a5ce71c77bb4c710b
David Basham <dbasham@overlake.org>
2019-01-29 09:14:57 -0800

    add questions

--- app/src/main/java/org/overlake/geoquiz/MainActivity.java
+++ app/src/main/java/org/overlake/geoquiz/MainActivity.java
@@ -5,20 +5,34 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[]{
+        new Question(R.string.question_one, true),
+        new Question(R.string.question_two, false),
+        new Question(R.string.question_three, true),
+        new Question(R.string.question_four, true),
+        new Question(R.string.question_five, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mFalseButton = (Button) findViewById(R.id.false_button);
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int questionId = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(questionId);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
