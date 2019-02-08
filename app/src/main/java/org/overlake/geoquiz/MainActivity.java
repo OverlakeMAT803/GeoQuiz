5340478b9dc201e7f586fdd56cc42ce61b9557ee
David Basham <dbasham@overlake.org>
2019-02-08 08:51:09 -0800

    final day of Geoquiz

--- app/src/main/java/org/overlake/geoquiz/MainActivity.java
+++ app/src/main/java/org/overlake/geoquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package org.overlake.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,9 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CURRENT_INDEX = "current_index";
+    private static final String TAG = "MainActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -28,12 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(CURRENT_INDEX, 0);
+        }
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
         mNextButton = findViewById(R.id.next_button);
+
+        findViewById(R.id.cheat_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CheatActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mQuestionTextView = findViewById(R.id.question_text_view);
         updateQuestion();
@@ -75,5 +92,41 @@ public class MainActivity extends AppCompatActivity {
             messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(CURRENT_INDEX,mCurrentIndex);
     }
 }
